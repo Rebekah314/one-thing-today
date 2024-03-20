21d6751538db9e2f96a2264a551bde1aecb19f28
Rebekah Garris <rebekahlgarris@gmail.com>
2024-03-19 19:28:25 -0500

    adding spring boot app with java backend

---
+++ OneThingTodayApp/src/main/java/org/launchcode/OneThingTodayApp/OneThingTodayApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.OneThingTodayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneThingTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneThingTodayApplication.class, args);
+	}
+
+}
---
+++ OneThingTodayApp/src/test/java/org/launchcode/OneThingTodayApp/OneThingTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.OneThingTodayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneThingTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
