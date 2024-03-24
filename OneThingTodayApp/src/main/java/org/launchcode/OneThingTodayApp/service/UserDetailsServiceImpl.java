5c9a67976043e181799ca0118d1cc4f9097337f2
Rebekah Garris <rebekahlgarris@gmail.com>
2024-03-23 21:23:30 -0500

    working on spring security still. encoding passwords.

--- OneThingTodayApp/src/main/java/org/launchcode/OneThingTodayApp/service/UserDetailsServiceImpl.java
+++ OneThingTodayApp/src/main/java/org/launchcode/OneThingTodayApp/service/UserDetailsServiceImpl.java
@@ -1,13 +1,18 @@
 package org.launchcode.OneThingTodayApp.service;
 
 import org.launchcode.OneThingTodayApp.models.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -15,7 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		
 		//hard-coding a user to return (for now) [TODO update this]
 		member.setDisplayName(username);
-		member.setPassword("rebekah");
+		member.setPassword(passwordEncoder.encode("rebekah"));
 		member.setId(1L);
 		
 		return member;
